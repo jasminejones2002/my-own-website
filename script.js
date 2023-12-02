da54f7dbb2c0c1d81e3de0e2a4159622fd7700ae
Jasmine <jazziejones02@yahoo.com>
2023-12-02 00:26:41 -0600

    my own website updated

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const h2 =document.createElement("h2");
-h2.textContent = "This content added by Javascript";
+/*
+const h2 = document.createElement("h2");
+h2.textContent = "Who is Jasmine?";
 document.querySelector("body").appendChild(h2);
+h2.remove();
+*/
+function clickHere(funsurprise) {
+   const button = document.getElementById("HERE").click();
+   button.addEventListener('click', function() {
+    const link = document.createElement('a');
+    link.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    link.textcontent = "HERE";
+    document.body.appendChild(link);
+   });
+}
+function changeStyle() {
+    let bigImage = document.getElementsByClassName("wsiblings")
+    bigImage.addEventListener( "mouseover", function() {
+        document.wsiblings.style = "height:200px; width:200px";
+    });
+}
