86b73c0c8c4c0ad517802ca91b3b7890af18f4e2
Jasmine <jazziejones02@yahoo.com>
2023-12-04 00:37:24 -0600

    zoom affcts added

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ h2.textContent = "Who is Jasmine?";
 document.querySelector("body").appendChild(h2);
 h2.remove();
 */
-function clickHere(funsurprise) {
-   const button = document.getElementById("HERE").click();
-   button.addEventListener('click', function() {
-    const link = document.createElement('a');
-    link.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    link.textcontent = "HERE";
-    document.body.appendChild(link);
+
+document.addEventListener("DOMContentLoaded", function() {
+   const images = document.querySelectorAll(".zoomable-image");
+ 
+   images.forEach(function(image) {
+     image.addEventListener("click", function() {
+       image.classList.toggle("zoomed");
+      });
    });
-}
+});
